12c10013a60b7be888ecc3458d7e92b813a8633e
chrispetito <christopherpetito@gmail.com>
2019-04-02 14:15:26 -0700

    fix problem 1 function invokation so that it does not return an undefined; complete some of the stretch challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,21 +13,26 @@ function firstFunction() {
   }secondFunction();
 };
 
-console.log(firstFunction());
+firstFunction();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+var a = 0;
+const counter = function() {
+  console.log(a += 1);
 };
+  // Return a function that when invoked increments and returns a counter variable.
+counter();
+counter();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = function() {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
